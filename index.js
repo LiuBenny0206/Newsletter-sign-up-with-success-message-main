f0c5b79ac599df88c3563a497a1be81417be241f
LiuBenny0206 <karta30121@gmail.com>
2024-03-05 20:29:12 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    var emailInput = document.getElementById('userEmail');
+    var errorSpan = document.getElementById('emailError');
+
+    // 检查邮箱是否有效
+    if (emailInput.validity.valid) {
+        // 如果邮箱有效，隐藏错误消息
+        document.getElementById('emailError').hidden = true;
+    } else {
+        // 如果邮箱无效，阻止表单提交并显示错误消息
+        event.preventDefault();
+        document.getElementById('emailError').hidden = false;
+
+    }
+});
+
+var button = document.getElementById("subscribe");
+
+button.style.backgroundColor = ' hsl(234, 29%, 20%)';
+
+function handleMouseOver(){
+    button.style.backgroundColor = 'hsl(4, 100%, 67%)';
+}
+
+function handleMouseOut(){
+    button.style.backgroundColor = ' hsl(234, 29%, 20%)';
+}
+
+button.addEventListener('mouseover' , handleMouseOver);
+button.addEventListener('mouseout', handleMouseOut);
